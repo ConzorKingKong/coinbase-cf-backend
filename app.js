83bfd11d91427c11063a089a05ba9958ebb99d65
Connor Peshek <connorpeshek@gmail.com>
2017-09-25 21:40:40 -0400

    Update

--- app.js
+++ app.js
@@ -2,35 +2,27 @@ var express = require("express")
 var bodyParser = require("body-parser")
 var app = express()
 var axios = require("axios")
+var PORT = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 
-app.post('/', function(req, res) {
-  console.log("install", req.body.install)
-
-  axios.get("https://api.coinbase.com/v2/checkouts", {
+app.post('/', function(request, response) {
+  axios.get("https://api.coinbase.com/v2/accounts", {
     headers: {
-      Authorization: "Bearer " + req.body.authentications.account.token.token
+      Authorization: "Bearer " + request.body.authentications.account.token.token
     }
   })
-  .then(function(res, err) {
-    if (res) {
-      // res.data.data is the list of their current sales available
-      console.log(res.data)
+  .then(function(promiseResponse, error) {
+    if (promiseResponse) {
+      request.body.install.options.bitcoin = parseFloat(promiseResponse.data.data[0].balance.amount)
+      response.status(200).send({install: request.body.install, proceed: true})
     }
   })
-
-  res.status(200).send({install: req.body.install, proceed: true})
+  .catch(function(error) {
+    console.log("error ", error)
+  })
 })
 
-// app.post("/create", function(req, res) {
-//   axios.post("https://api.coinbase.com/v2/checkouts", {
-
-//   },{
-//     headers: {
-//       Authorization: "Bearer " + req.body.authentications.account.token.token
-//     }
-//   })
-// })
-
-app.listen(4568)
+app.listen(PORT, function() {
+  console.log("running on port " + PORT)
+})
