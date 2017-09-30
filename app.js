7c52203164c0f2c48965d3f600331016a5283cc3
Connor Peshek <connorpeshek@gmail.com>
2017-09-29 20:35:05 -0400

    Clean up code.

--- app.js
+++ app.js
@@ -7,23 +7,20 @@ var PORT = process.env.PORT || 3000
 app.use(bodyParser.json())
 
 app.post('/', function(request, response) {
-  var type = request.body.authentications.account.token.type
-  type = type.charAt(0).toUpperCase() + type.slice(1)
-
+  
   var token = request.body.authentications.account.token.token
 
   simpleFetch.getJson("https://api.coinbase.com/v2/accounts", {
     headers: {
-      Authorization: `${type} ${token}`
+      Authorization: `Bearer ${token}`
     }
   })
-  .then(function(promiseResponse, error) {
-    if (promiseResponse) {
+  .then(function(account) {
       var options = request.body.install.options
-      var amount = promiseResponse.data[0].balance.amount
+      var amount = account.data[0].balance.amount
+      
       options.bitcoin = parseFloat(amount)
       response.json({install: request.body.install, proceed: true})
-    }
   })
 })
 
